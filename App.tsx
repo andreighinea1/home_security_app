e84013d9a56281518bb31b3283d4967e054ac912
aghinea <aghinea@bitdefender.com>
2023-06-10 18:22:55 +0300

    Started building

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import {Box, Text, Button, GluestackUIProvider} from "./components"
+import {config} from "./gluestack-ui.config"
+import {Amplify} from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+
+export default function App() {
+    return (
+        <GluestackUIProvider config={config.theme}>
+            <Box bg="$primary500" p="$5">
+                <Text color="white">This is the Box</Text>
+                <Button onPress={() => alert("Clicked!")}>
+                    <Button.Text>Click me!</Button.Text>
+                </Button>
+            </Box>
+        </GluestackUIProvider>
+    )
+}
