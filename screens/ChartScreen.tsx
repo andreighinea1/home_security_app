98fd3a7d918c066674d72344d2597cdfd2fd36a4
aghinea <aghinea@bitdefender.com>
2023-06-12 17:13:38 +0300

    Added a pre-building of android app Added something in the ChartScreen

--- screens/ChartScreen.tsx
+++ screens/ChartScreen.tsx
@@ -1,12 +1,54 @@
 import {Box, GluestackUIProvider, Text} from "../components";
 import {config} from "../gluestack-ui.config";
 import {styles} from "../my-styles";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 
 const ChartScreen = () => {
+    const data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+            {
+                data: [20, 45, 28, 80, 99, 43],
+                color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+                strokeWidth: 2 // optional
+            }
+        ],
+        legend: ["Rainy Days"] // optional
+    };
+    const screenWidth = Dimensions.get("window").width;
+    const chartConfig = {
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#fb8c00",
+        backgroundGradientTo: "#ffa726",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+            borderRadius: 16
+        },
+        propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+        }
+    }
+
     return (
         <GluestackUIProvider config={config.theme}>
             <Box>
-                <Text color='$amber50'>This is the Chart Screen.</Text>
+                <LineChart
+                    data={data}
+                    width={screenWidth}
+                    height={220}
+                    chartConfig={chartConfig}
+                />
             </Box>
         </GluestackUIProvider>
     );
